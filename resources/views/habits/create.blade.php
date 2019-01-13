ca74f8b00985344bc89931de350a8ec026186bb0
Paulo Chullmir <pchullmir@MacBook-Pro-de-Paulo.local>
2019-01-13 01:39:01 -0300

    primer commit

---
+++ resources/views/habits/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('contenido')
+<form action="">
+	@csrf
+	<div class="form-group">
+		<p class="title">Fume</p>
+		<input type="radio">
+	</div>
+</form>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('contenido')
+<a href="{{route('habits.create')}}">Cargar Habito</a>
+@endsection
